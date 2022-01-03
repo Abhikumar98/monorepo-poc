8730dbd206b0472c2ef3654c87b240da902d4ffb
abhikumar98 <abhishekkumar35962@gmail.com>
2022-01-03 15:54:16 +0530

    fix: first things

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <button>
+            Button
+        </button>
+    )
+}
+
+export default Button
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import Button from "./Button";
+export { NewTabLink } from "./NewTabLink";
+export { CounterButton } from "./CounterButton";
+export { Button };
